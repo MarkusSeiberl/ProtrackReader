77f772569a1dc3d128ff284a4cd82c591089fd1a
markus <markus_seiberl@live.at>
2025-02-17 21:21:22 +0100

    Add IME actions to create pdf jump dialog

--- app/src/main/java/com/seiberl/protrackreader/ui/home/PrintJumpsDialog.kt
+++ app/src/main/java/com/seiberl/protrackreader/ui/home/PrintJumpsDialog.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialogDefaults
@@ -31,9 +32,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.seiberl.protrackreader.R
@@ -96,6 +100,8 @@ fun PrintJumpsDialog(
                     null
                 }
 
+                val focusManager = LocalFocusManager.current
+
                 OutlinedTextField(
                     value = jumpNrFrom,
                     onValueChange = { newValue -> jumpNrFrom = newValue },
@@ -103,7 +109,13 @@ fun PrintJumpsDialog(
                     supportingText = supportedFromText,
                     textStyle = LocalTextStyle.current,
                     maxLines = 1,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Next
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onNext = { focusManager.moveFocus(FocusDirection.Down) }
+                    ),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(12.dp),
@@ -116,7 +128,18 @@ fun PrintJumpsDialog(
                     supportingText = supportedToText,
                     textStyle = LocalTextStyle.current,
                     maxLines = 1,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            focusManager.clearFocus()
+                            onCreatePdfOfJumps(
+                                jumpNrFrom, jumpNrTo
+                            )
+                        }
+                    ),
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(12.dp),
